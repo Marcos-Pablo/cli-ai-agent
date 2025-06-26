72bec2ac8cbc36dd06e0e663bb62df9e9b1f5213
Marcos Pablo <marquin.pablo@gmail.com>
2025-06-26 20:51:03 -0300

    refactoring main

--- main.py
+++ main.py
@@ -6,27 +6,30 @@ from google.genai import types
 
 def main():
     if len(sys.argv) < 2:
-        print("Error: The prompt is required. Please provide at least one argument.")
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    verbose = False
-    for i in range(2, len(sys.argv)):
-        if sys.argv[i] == "--verbose":
-            verbose = True
-
-    user_prompt = sys.argv[1]
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-    ]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
+    user_prompt = " ".join(sys.argv[1:])
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    generate_content(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
         contents=messages,
     )
 
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
